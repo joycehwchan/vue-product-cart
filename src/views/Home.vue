6ea3378512ca495e3631cada62fbfb7210ef3b36
joycehwchan <joycehwchan@gmail.com>
2023-01-15 16:07:34 +0900

    refactor product-card component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,56 +10,26 @@
       <h2>Recommended</h2>
 
       <div class="recommended">
-        <div
-          v-for="(product, i) in inventory.slice(0, 3)"
+        <ProductCard
+          v-for="(product, index) in inventory.slice(0, 3)"
+          :index="index"
+          :product="product"
+          :addToCart="addToCart"
           :key="product.id"
-          class="card"
-        >
-          <div class="card-title">
-            {{ product.name }}
-          </div>
-          <div class="card-body">
-            <i class="icofont-10x icofont-{{ product.icon }}"></i>
-            <form>
-              <div class="row">
-                <div class="cell">
-                  <label>Type:</label>
-                </div>
-                <div class="cell">
-                  <em>{{ product.type }}</em>
-                </div>
-              </div>
-              <div class="row">
-                <div class="cell">
-                  <label>Price:</label>
-                </div>
-                <div class="cell">${{ product.price.USD }}</div>
-              </div>
-              <div class="row">
-                <div class="cell">
-                  <label>Quantity:</label>
-                </div>
-                <div class="cell">
-                  <input type="number" v-model.number="product.quantity" />
-                </div>
-              </div>
-            </form>
-          </div>
-          <div class="card-footer">
-            <button @click="addToCart(product.name, i)" class="btn btn-light">
-              Add to cart
-            </button>
-          </div>
-        </div>
+        />
       </div>
     </main>
   </div>
 </template>
 
 <script>
+import ProductCard from '../components/ProductCard.vue'
 // prettier-ignore
 export default {
   name: 'Home',
-  props: ['inventory', 'addToCart']
+  props: ['inventory', 'addToCart'],
+  components: {
+    ProductCard
+  }
 }
 </script>
